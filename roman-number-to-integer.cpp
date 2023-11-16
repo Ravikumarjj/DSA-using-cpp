8d276082526c941ac791ea21bdf60650fde60be1
Ravi kanu baniya <geniusk53@gmail.com>
2023-11-16 19:55:30 +0530

    roman to integer

---
+++ roman-number-to-integer.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int romanToDecimal(string &str) {
+        // code here
+        unordered_map<char,int>mp;
+        mp['I']=1;
+        mp['V']=5;
+        mp['X']=10;
+        mp['L']=50;
+        mp['C']=100;
+        mp['D']=500;
+        mp['M']=1000;
+       int sum=0;
+       int pre=0;
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            if(mp[str[i]] < pre) sum-=mp[str[i]];
+            else sum+=mp[str[i]];
+            pre=mp[str[i]];
+        }
+        return sum;
+    }
+};
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        Solution obj;
+        cout<<obj.romanToDecimal(s)<<endl;
+    }
+    return 0;
+}
