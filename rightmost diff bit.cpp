f9ba54009656c9847979dc1408ec5443e4200cde
Ravi kanu baniya <geniusk53@gmail.com>
2023-12-19 13:52:40 +0530

    Day 42 of #100DaysOfCode

---
+++ rightmost diff bit.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+    int posOfRightMostDiffBit(int m, int n)
+    {
+        // Your code here
+        if(m==n) return -1;
+            int ans = m^n;
+           
+            for(int i=0;i<n;i++){
+               if(ans&(1<<i)) {
+                   return i+1;
+               }
+            }
+        
+           return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{   
+    int t;
+    cin>>t; //input number of testcases
+    while(t--)
+    {
+         int m,n;
+         cin>>m>>n; //input m and n
+         Solution ob;
+         cout << ob.posOfRightMostDiffBit(m, n)<<endl;
+    }
+    return 0;     
+} 
+// } Driver Code Ends
