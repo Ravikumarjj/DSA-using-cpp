525384400a5f5eddf853714ad3c2e36c3de1953a
Ravi kanu baniya <geniusk53@gmail.com>
2024-02-05 11:42:44 +0530

    diameter of BT

---
+++ Diameter of BT.cpp
@@ -0,0 +1,39 @@
+//Diameter is nothing but the longest path between any two nodes 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+
+    int height(TreeNode<int>* node, int& diameter) {
+
+        if (!node) {
+            return 0;
+        }
+
+        int lh = height(node->left, diameter);
+        int rh = height(node->right, diameter);
+
+        diameter = max(diameter, lh + rh);
+
+        return 1 + max(lh, rh);
+    }
+     int diameterOfBinaryTree(TreeNode<int>* root) {
+        int diameter = 0;
+        height(root, diameter);
+        return diameter;
+    }
+
