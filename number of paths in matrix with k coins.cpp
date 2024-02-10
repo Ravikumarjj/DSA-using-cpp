9ba108100bdb331f0001fc854f9dae176d63e919
Ravi kanu baniya <geniusk53@gmail.com>
2024-02-10 22:06:56 +0530

    Day 95 of #100DaysOfCode

---
+++ number of paths in matrix with k coins.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+public:
+    
+    long long f(int row, int col, int ans, vector<vector<int>>& arr, vector<vector<int>>& vis, vector<vector<vector<long long>>>& memo, int n) {
+        if (row == n - 1 && col == n - 1) return (ans == arr[n - 1][n - 1]);
+        if (row >= n || col >= n) return 0;
+        if (memo[row][col][ans] != -1) return memo[row][col][ans];
+
+        long long up = 0;
+        long long left = 0;
+        if (vis[row][col] == 0 && ans >= arr[row][col]) {
+            vis[row][col] = 1;
+            up = f(row + 1, col, ans - arr[row][col], arr, vis, memo, n);
+            left = f(row, col + 1, ans - arr[row][col], arr, vis, memo, n);
+            vis[row][col] = 0;
+        }
+
+        memo[row][col][ans] = up + left;
+        return memo[row][col][ans];
+    }
+
+    long long numberOfPath(int n, int k, vector<vector<int>>& arr) {
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        vector<vector<vector<long long>>> memo(n, vector<vector<long long>>(n, vector<long long>(k + 1, -1))); // memoization table
+
+        return f(0, 0, k, arr, vis, memo, n);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    Solution obj;
+    int i,j,k,l,m,n,t;
+    cin>>t;
+    while(t--)
+    {
+        cin>>k>>n;
+        vector<vector<int>> v(n, vector<int>(n, 0));
+        for(i=0;i<n;i++)
+            for(j=0;j<n;j++)
+                cin>>v[i][j];
+        cout << obj.numberOfPath(n, k, v) << endl;
+    }
+}
+// } Driver Code Ends
