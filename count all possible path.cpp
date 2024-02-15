2d94a21cfb96a5db6ea10fbb6dc1a107b6f33792
Ravi kanu baniya <geniusk53@gmail.com>
2024-02-15 11:17:29 +0530

    Day 100 of #100DaysOfCode

---
+++ count all possible path.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+public:
+	int isPossible(vector<vector<int>>paths){
+	    // each vertex will have an even degree for Eulerian Circuits
+	    int n = paths.size();
+	    for(int i = 0; i < n; i++) {
+	        int degree = 0;
+	        for(int j = 0; j < n; j++) degree+=paths[i][j];
+	        if(degree%2 != 0)
+	            return false;
+	    }
+	    return true;
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>paths(n, vector<int>(n, 0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++)
+				cin >> paths[i][j];
+		}
+		Solution obj;
+		int ans = obj.isPossible(paths);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
