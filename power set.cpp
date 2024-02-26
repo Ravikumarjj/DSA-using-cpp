ce86c706d27d7c732bc5da933b24da8e017bb7a8
Ravi kanu baniya <geniusk53@gmail.com>
2024-02-26 14:54:13 +0530

    power set | Day 111

---
+++ power set.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+		vector<string> AllPossibleStrings(string s){
+		    // Code here
+		     int n = s.length();
+		    vector<string> ans;
+		    
+		    for(int i = 1;i < (1<<n); i++) {
+		        string subStr = "";
+		        for(int j = 0;j < n; j++) {
+		            if(i & (1<<j))
+		                subStr += s[j];
+		        }
+		        ans.push_back(subStr);
+		    }
+		    
+		    sort(ans.begin(), ans.end());
+		    return ans;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string s;
+		cin >> s;
+		Solution ob;
+		vector<string> res = ob.AllPossibleStrings(s);
+		for(auto i : res)
+			cout << i <<" ";
+		cout << "\n";
+
+	}
+	return 0;
+}
+// } Driver Code Ends
